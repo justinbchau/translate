e730b39ec11e9adade78ea61cffb2a18a1c1df29
mrchaufun <46144954+mrchaufun@users.noreply.github.com>
2020-02-23 08:29:36 -0800

    Finished

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 export default class Button extends Component {
+
+    renderSubmit(language) {
+        return language === 'english' ? 'Submit' : 'Voorleggen';
+    }
+
     render() {
         return (
-            <button className="ui primary button">
-                Submit
-            </button>
+            <ColorContext.Consumer>
+                {(color) => 
+                <button className={`ui ${color} button`}>
+                    <LanguageContext.Consumer>
+                        {({ language }) => this.renderSubmit(language)}
+                    </LanguageContext.Consumer>
+                </button>
+                }
+                
+            </ColorContext.Consumer>
         )
     }
 }
